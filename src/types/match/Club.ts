5898c3fafab1fe6feca052474ddcb78fd1fa0178
AlanGliksberg <alan.gliksberg@globons.com>
2025-11-25 13:08:16 -0300

    tiop de club

--- src/types/match/Club.ts
+++ src/types/match/Club.ts
@@ -1,9 +1,6 @@
 export type Club = {
   id: number;
   name: string;
-  address?: string | null;
-  city?: string | null;
-  province?: string | null;
-  latitude?: number | null;
-  longitude?: number | null;
+  description: string;
+  enabled: boolean;
 };
